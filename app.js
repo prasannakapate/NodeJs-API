43275db6174bab30ab56f56e3bc9371cc01b84da
Prasanna Kapate <prasanna.1616@gmail.com>
2016-05-23 12:02:47 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+var express = require('express'),
+  mongoose = require('mongoose');
+
+var db = mongoose.connect('mongodb://demo:demo@ds025802.mlab.com:25802/bookapi');
+var Book = require('./models/bookModel');
+
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+var bookRouter = express.Router();
+
+app.use('/api', bookRouter);
+
+bookRouter.route('/Books')
+  .get(function(req, res) {
+    Book.find(function(err, books) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.json(books);
+      }
+    });
+    res.json(responseJson);
+  });
+app.get('/', function(req, res) {
+  res.send('welcome to my API');
+});
+
+app.listen(port, function() {
+  console.log('Gulp is running my app on PORT: ' + port);
+});
